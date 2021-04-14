b0e9fe7935e3a02d692efebf05f3c710a496c7e1
Robert Erdin <robert.erdin@gmail.com>
2021-04-14 22:04:57 +0100

    minor adjustments

--- keyboards/sofle/keymaps/roberterdin/keymap.c
+++ keyboards/sofle/keymaps/roberterdin/keymap.c
@@ -23,14 +23,14 @@ enum custom_keycodes {
 
 
 // Tap Dance declarations
-enum {
-    TD_SFT_CAPS,
-};
+// enum {
+//     TD_SFT_CAPS,
+// };
 
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),ASD
-};
+// // Tap Dance definitions
+// qk_tap_dance_action_t tap_dance_actions[] = {
+//     [TD_SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),ASD
+// };
 
 int chars_pressed = 0;
 
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   LCTL_T(KC_ESC),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  TD(TD_SFT_CAPS),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_MINS), \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_MINS), \
                 KC_BSLS, KC_LGUI,KC_LALT, KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, ALGR_T(KC_LBRC), KC_RBRC, KC_EQL \
 ),
 
@@ -712,4 +712,3 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 //     oled_write_raw_P(nyan_cat, ANIM_SIZE);
 // }
-o
